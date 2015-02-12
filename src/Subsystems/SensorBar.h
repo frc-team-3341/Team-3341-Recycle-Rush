1b0f91df156888d1254b5119051742fa1c02f01a
Ronak <ronakshahultimate@gmail.com>
2015-02-11 19:56:58 -0800

    sensor bar

---
+++ src/Subsystems/SensorBar.h
@@ -0,0 +1,19 @@
+#ifndef SensorBar_H
+#define SensorBar_H
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+#include <I2C.h>
+
+
+class SensorBar: public Subsystem
+{
+private:
+	// It's desirable that everything possible under private except
+	// for methods that implement subsystem capabilities
+public:
+	SensorBar();
+	void InitDefaultCommand();
+};
+
+#endif
