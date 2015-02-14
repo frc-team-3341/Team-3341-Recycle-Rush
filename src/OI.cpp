b66b5a97a4b93a81eb68899cde4d95f93d666ebf
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2015-02-13 22:42:38 -0800

    Wrote CVAlignTote command
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- src/OI.cpp
+++ src/OI.cpp
@@ -1,14 +1,19 @@
 #include "OI.h"
 #include "Commands/ClawOn.h"
 #include "Commands/ClawOff.h"
-
+#include "Commands/CVAlignTote.h"
 
 OI::OI() :
  driveStick(new Joystick(DRIVESTICK)), operatorStick(new Joystick(OPERATORSTICK))
 {
+	// Claw control
 	Button* trigger = new JoystickButton(operatorStick, 1);
 	trigger->WhenPressed(new ClawOn());
 	trigger->WhenReleased(new ClawOff());
+
+	// CV auto-align control
+	Button* cvaligntote_button = new JoystickButton(operatorStick, 4);
+	cv_go->ToggleWhenPressed(new CVAlignTote());
 	// Process operator interface input here.
 }
 
