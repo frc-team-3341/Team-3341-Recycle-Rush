1ea0b486a0e641f9cd8d6b7347abd6f171e0e0c6
Prem Pathuri <prem1835213@gmail.com>
2015-02-16 17:14:12 -0800

    New Automove command

---
+++ src/Commands/CVAlignTote.h
@@ -0,0 +1,13 @@
+#ifndef CVAlignTote_H
+#define CVAlignTote_H
+
+#include "Commands/CommandGroup.h"
+#include "WPILib.h"
+
+class CVAlignTote: public CommandGroup
+{
+public:
+	CVAlignTote();
+};
+
+#endif
